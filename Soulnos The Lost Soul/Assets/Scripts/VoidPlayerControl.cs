5758ce1eb5c76518add55a74c29ee339654eb2a8
tohoilam <61353084+tohoilam@users.noreply.github.com>
2021-03-09 17:47:41 +0800

    Imported 2d background and added player and collision

---
+++ Soulnos The Lost Soul/Assets/Scripts/VoidPlayerControl.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoidPlayerControl : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+
+    bool isGrounded;
+    new Rigidbody2D rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isGrounded = false;
+        rigidbody = this.GetComponent<Rigidbody2D>();
+        this.rigidbody.gravityScale *= -1;
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        isGrounded = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.Translate(Time.deltaTime * speed, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.Translate(-Time.deltaTime * speed, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && !isGrounded)
+        {
+            this.rigidbody.AddForce(new Vector3(0.0f, 1.0f, 0.0f) * -jumpForce, ForceMode2D.Impulse);
+        }
+    }
+}
