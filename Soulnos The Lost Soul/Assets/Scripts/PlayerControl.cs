b3824be31350314675a810512d86cd867f855630
tohoilam <61353084+tohoilam@users.noreply.github.com>
2021-03-15 01:28:16 +0800

    Added Idle and Running Animation

--- Soulnos The Lost Soul/Assets/Scripts/PlayerControl.cs
+++ Soulnos The Lost Soul/Assets/Scripts/PlayerControl.cs
@@ -9,12 +9,17 @@ public class PlayerControl : MonoBehaviour
 
     bool isGrounded;
     new Rigidbody2D rigidbody;
+    SpriteRenderer spriteRenderer;
+    Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         isGrounded = false;
         rigidbody = this.GetComponent<Rigidbody2D>();
+        spriteRenderer = this.GetComponent<SpriteRenderer>();
+        animator = this.GetComponent<Animator>();
+
     }
 
     void OnCollisionStay(Collision collision)
@@ -27,17 +32,26 @@ public class PlayerControl : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.D))
         {
-            this.transform.Translate(Time.deltaTime * speed, 0, 0);
+            spriteRenderer.flipX = false;
+            //this.rigidbody.MovePosition(transform.position + (Time.deltaTime * speed * new Vector3(1.0f, 0.0f, 0.0f)));
+            //this.transform.Translate(Time.deltaTime * speed, 0, 0);
+            this.rigidbody.velocity = new Vector2(speed, rigidbody.velocity.y);
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            this.transform.Translate(-Time.deltaTime * speed, 0, 0);
+            spriteRenderer.flipX = true;
+            //this.rigidbody.MovePosition(transform.position + (Time.deltaTime * -speed * new Vector3(1.0f, 0.0f, 0.0f)));
+            //this.transform.Translate(-Time.deltaTime * speed, 0, 0);
+            this.rigidbody.velocity = new Vector2(-speed, rigidbody.velocity.y);
         }
 
         if (Input.GetKeyDown(KeyCode.Space) && !isGrounded)
         {
             this.rigidbody.AddForce(new Vector3(0.0f, 1.0f, 0.0f) * jumpForce, ForceMode2D.Impulse);
         }
+
+        animator.SetFloat("Speed", Mathf.Abs(rigidbody.velocity.x));
+        
     }
 }
