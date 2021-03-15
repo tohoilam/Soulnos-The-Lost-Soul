5d2fda28c1017d7935a80d624885fd99789b0fa7
tohoilam <61353084+tohoilam@users.noreply.github.com>
2021-03-15 23:37:07 +0800

    Modified player movement method and added jump restriction and animation

--- Soulnos The Lost Soul/Assets/Scripts/PlayerControl.cs
+++ Soulnos The Lost Soul/Assets/Scripts/PlayerControl.cs
@@ -7,15 +7,20 @@ public class PlayerControl : MonoBehaviour
     public float speed;
     public float jumpForce;
 
-    bool isGrounded;
-    new Rigidbody2D rigidbody;
-    SpriteRenderer spriteRenderer;
-    Animator animator;
+    [SerializeField] private LayerMask groundLayerMask;
+
+    private new Rigidbody2D rigidbody;
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+    private float horizontalInput;
+    private bool activateJump;
+    private bool isGrounded;
 
     // Start is called before the first frame update
     void Start()
     {
         isGrounded = false;
+        activateJump = false;
         rigidbody = this.GetComponent<Rigidbody2D>();
         spriteRenderer = this.GetComponent<SpriteRenderer>();
         animator = this.GetComponent<Animator>();
@@ -30,28 +35,52 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.D))
+        horizontalInput = Input.GetAxis("Horizontal");
+        if (horizontalInput > 0)
         {
             spriteRenderer.flipX = false;
-            //this.rigidbody.MovePosition(transform.position + (Time.deltaTime * speed * new Vector3(1.0f, 0.0f, 0.0f)));
-            //this.transform.Translate(Time.deltaTime * speed, 0, 0);
-            this.rigidbody.velocity = new Vector2(speed, rigidbody.velocity.y);
         }
-
-        if (Input.GetKey(KeyCode.A))
+        else if (horizontalInput < 0)
         {
             spriteRenderer.flipX = true;
-            //this.rigidbody.MovePosition(transform.position + (Time.deltaTime * -speed * new Vector3(1.0f, 0.0f, 0.0f)));
-            //this.transform.Translate(-Time.deltaTime * speed, 0, 0);
-            this.rigidbody.velocity = new Vector2(-speed, rigidbody.velocity.y);
         }
 
-        if (Input.GetKeyDown(KeyCode.Space) && !isGrounded)
+        if (isGrounded && Input.GetButtonDown("Jump"))
         {
-            this.rigidbody.AddForce(new Vector3(0.0f, 1.0f, 0.0f) * jumpForce, ForceMode2D.Impulse);
+            activateJump = true;
         }
 
         animator.SetFloat("Speed", Mathf.Abs(rigidbody.velocity.x));
+
+    }
+
+    private void FixedUpdate()
+    {
+        this.rigidbody.velocity = new Vector2(speed * horizontalInput, rigidbody.velocity.y);
+        animator.SetFloat("Speed", Mathf.Abs(rigidbody.velocity.x));
         
+        if (activateJump)
+        {
+            animator.SetBool("IsJumping", true);
+            this.rigidbody.AddForce(new Vector2(0.0f, 1.0f) * jumpForce, ForceMode2D.Impulse);
+            activateJump = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider != null)
+        {
+            if (((1 << collider.gameObject.layer) & groundLayerMask) != 0)
+            {
+                isGrounded = true;
+                animator.SetBool("IsJumping", false);
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        isGrounded = false;
     }
 }
