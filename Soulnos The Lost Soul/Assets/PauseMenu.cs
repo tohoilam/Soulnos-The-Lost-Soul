7eaaf726cc55cbed7651fc103627b1dbc8f9a072
elvistsangouo <u3568549@connect.hku.hk>
2021-03-29 21:57:57 +0800

    Pause Menu is added to prefab
    For each scene, please manually add pause menu

---
+++ Soulnos The Lost Soul/Assets/PauseMenu.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{   
+    public static bool paused = false;
+    public GameObject pauseMenuUI;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (paused == true)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+    
+    void PauseGame()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0;
+        paused = true;
+    }
+    public void ResumeGame()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1;
+        paused = false;
+    }
+    public void QuitGame()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
